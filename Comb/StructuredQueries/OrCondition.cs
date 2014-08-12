07db86e170fbcbe6a388234bb892486279e022fc
Barry Dahlberg <barry.dahlberg@gmail.com>
2014-08-13 11:26:12 +1200

    Operators now take ICollection instead of IEnumberable.
    Guards against possible multiple enumeration of IEnumerable.

--- Comb/StructuredQueries/OrCondition.cs
+++ Comb/StructuredQueries/OrCondition.cs
@@ -1,12 +1,10 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Comb.StructuredQueries
 {
     public class OrCondition : Operator
     {
-        public OrCondition(IEnumerable<IOperand> operands, uint? boost = null, string field = null)
+        public OrCondition(ICollection<IOperand> operands, uint? boost = null, string field = null)
             : base("or", operands)
         {
             Field = field;
